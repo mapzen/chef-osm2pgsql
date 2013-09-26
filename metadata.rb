1b25b325b2b153c8a6a61947bbabe404cf8374e1
Grant Heffernan <heffergm@gmail.com>
2013-09-26 15:38:00 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures osm2pgsql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.4.1'
 
 %w{ apt git }.each do |dep|
     depends dep
