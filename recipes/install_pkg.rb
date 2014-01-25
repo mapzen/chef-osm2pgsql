5c3103cac9a05ba540d564ffa271ac98631acc37
Grant Heffernan <heffergm@gmail.com>
2014-01-25 13:41:59 -0500

    pkg from apt

--- recipes/install_pkg.rb
+++ recipes/install_pkg.rb
@@ -7,15 +7,5 @@
 # All rights reserved - Do Not Redistribute
 #
 
-apt_repository 'mapzen-public' do
-  uri           'http://s3.amazonaws.com/mapzen-debian'
-  distribution  'stable'
-  components    ['main']
-  key           'https://s3.amazonaws.com/mapzen-debian/mapzen-debian.gpg.key'
-end
-
-package 'mapzen-osm2pgsql' do
-  action  :install
-  version node[:osm2pgsql][:pkg_version]
-end
+package 'osm2pgsql'
 
