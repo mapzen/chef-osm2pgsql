8c4d0a6009857e5c35e7e69f701d47250133b0bd
Robert Marianski <rob@marianski.com>
2016-08-22 14:57:22 -0400

    Add symlink to normalize path across install types

--- recipes/install_pkg.rb
+++ recipes/install_pkg.rb
@@ -8,3 +8,8 @@ include_recipe 'osm2pgsql::install_ppa' if node[:osm2pgsql][:ppa][:enabled]
 package 'osm2pgsql' do
   action node[:osm2pgsql][:upgrade] ? :upgrade : :install
 end
+
+link '/usr/local/bin/osm2pgsql' do
+  to '/usr/bin/osm2pgsql'
+  link_type :symbolic
+end
